7f4fe79531f7e43bd569f78f36b4d639c925fa6a
Yonggan <yonggan@obco.pro>
2024-04-17 21:48:01 +0200

    Add drift web

--- lib/cachemanager_web.dart
+++ lib/cachemanager_web.dart
@@ -2,9 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:flutter_map_cache/flutter_map_cache.dart';
 import 'package:kifferkarte/cachemanager_stub.dart';
+import 'package:dio_cache_interceptor_db_store/dio_cache_interceptor_db_store.dart';
 
 class CacheManager extends BaseCacheManager {
   Future<CacheStore> getCacheStore() async {
-    return MemCacheStore();
+    return DbCacheStore(
+      databasePath: '', // ignored on web
+      databaseName: 'DbCacheStore',
+    );
+    // return MemCacheStore();
   }
 }
